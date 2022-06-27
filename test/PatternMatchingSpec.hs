10252820e222e54dd2aeca6c414e81c13af15261
pascualmg <pascual.munoz@pccomponentes.com>
2022-06-27 17:40:08 +0200

    fix test

--- test/PatternMatchingSpec.hs
+++ test/PatternMatchingSpec.hs
@@ -9,9 +9,9 @@ spec =  do
   describe "PatternMatching.isZero" $ do
     it "returns True if the element is Number 0" $ do
       print $ isZero 0
-      SUT.isZero 0 `shouldBe` False
+      SUT.isZero 0 `shouldBe` True
     it "returns False if the element is Number 1" $ do
       print $ isZero 1
-      SUT.isZero 0 `shouldBe` False
+      SUT.isZero 1 `shouldBe` False
     it "throws an exception if used with an empty list" $ do
       evaluate (head []) `shouldThrow` anyException
