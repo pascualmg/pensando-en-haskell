5ed98bcd62686fec130257855e44a9e3bd368608
pascual.munoz <pascual.munoz@pccomponentes.com>
2022-07-05 01:05:51 +0200

    1.5,6 done

---
+++ src/Exercises/Tema01/EJ05/LastCipherOfOneNumber.hs
@@ -0,0 +1,4 @@
+module Exercises.Tema01.EJ05.LastCipherOfOneNumber where
+
+minorWeight :: Int -> Int
+minorWeight n = rem n 10 
---
+++ src/Exercises/Tema01/EJ06/BiggerOfTrhee.hs
@@ -0,0 +1,5 @@
+module Exercises.Tema01.EJ06.BiggerOfTrhee where
+  
+  biggerOfTrhee :: Int -> Int -> Int -> Int
+  biggerOfTrhee a b c = maximum [a, b, c]
+   
