fcc65ea73d48a35ce41213dd7e76bac3ff1f5f0b
Pascual Muñoz Galian <pmunozg@ces.vocento.com>
2025-11-21 18:19:55 +0100

    snapshot guay

--- app/Main.hs
+++ app/Main.hs
@@ -3,12 +3,32 @@
 module Main where
 
 import ADNCalculator (countADN, isValid)
+import Control.Monad (forM_)
+import Exercises.Tema01.Ej01.MediaDeTresNumeros (media3)
+import Network.HTTP.Simple
 
 main :: IO ()
 main = do
   print $ isValid "ADN"
   print $ countADN "GATTACAGATTACAGATTTTACCCAAAAA"
-
+  forM_
+    [ "GATTACCCCCCAGGGG"
+    , "AAAAATTTTTCCCCCGGGGG"
+    , "ACGTACGTACGT"
+    ]
+    ( \adn -> do
+        let counted = countADN adn
+        putStrLn $ "Original: " ++ adn
+        putStrLn $ "Counted: " ++ show counted
+        putStrLn $ "Reconstructed: " ++ pinta counted
+        putStrLn "we"
+    )
+    mapM_
+    print
+    [ media3 2 3 1
+    , media3 10 20 30
+    , media3 (-5) 0 5
+    ]
 
 pinta :: [(Char, Int)] -> String
 pinta [] = ""
@@ -16,3 +36,9 @@ pinta ((c, i) : xs) = replicate i c ++ pinta xs
 
 -- >>> pinta $ countADN "GATTACCCCCCAGGGG"
 -- "AAATTCCCCCCGGGGG"
+
+-- >>> pinta [('A',5),('C',5),('G',57)]
+-- "AAAAACCCCCGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG"
+
+-- >>> media3 2 3 1
+-- 2.0
